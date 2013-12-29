0394467d38ef833e7b29c59b2bf75a60aa58a47e
Ryan Ringler <rringler@gmail.com>
2013-12-29 13:32:08 -0800

    Adds recent(n) and most_recent scopes to the Sample model, so smaller datasets can be plotted.

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,3 +1,6 @@
 class Sample < ActiveRecord::Base
   belongs_to :plant
+
+  scope :recent, ->(limit = 30) { order('created_at DESC').limit(limit) }
+  scope :most_recent, -> { order('created_at DESC').limit(1) }
 end
