f280f8dc340bae49394e601dbe1e523b4d2d8279
Ryan Ringler <rringler@gmail.com>
2014-01-12 00:16:51 -0800

    Fills out plants#edit integration testing.

--- spec/features/plant_edit_spec.rb
+++ spec/features/plant_edit_spec.rb
@@ -7,15 +7,33 @@ describe 'plants#edit' do
     visit root_path
     click_link "#{plant.name}"
     click_link "Edit"
-
-    save_and_open_page
   end
 
   it 'should have a title' do
     page.should have_css("h2", text: "Edit plant")
   end
 
-  it 'should render the plants form' do
-    page.should have_css('input[type="text"]', text: "#{plant.name}")
+  it 'should have a text input prefilled with the plant\'s name' do
+    page.should have_css("input#plant_name", exact: plant.name)
+  end
+
+  it 'should have a select input for the signal power pin' do
+    page.should have_css("select#plant_signal_power_pin", exact: plant.signal_power_pin)
+  end
+
+  it 'should have a select input for the signal adc channel' do
+    page.should have_css("select#plant_signal_channel", exact: plant.signal_channel)
+  end
+
+  it 'should have a select input for the pump power pin' do
+    page.should have_css("select#plant_pump_power_pin", exact: plant.pump_power_pin)
+  end
+
+  it 'should have a slider input for the plant\'s moisture threshold' do
+    page.should have_css("input#plant_moisture_threshold", exact: plant.moisture_threshold)
+  end
+
+  it 'should have a submit button' do
+    page.should have_css("button", exact: "Save")
   end
 end
